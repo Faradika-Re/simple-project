e18f4903b60c390054b949c385724903d3a36e61
Fra._da <wemusttellthemthatwecan61@gmail.com>
2025-08-26 00:02:11 +0700

    Test your luck here!
    There is a Python program which will test your luck.
    Just remember the drop-rate is still 50/50.

---
+++ Lucky.py
@@ -0,0 +1,15 @@
+import random
+
+# i wanna create a program that generates random word from a array
+while True:
+    choice = input("Do you want to try your luck? (yes/no): ").lower()
+    if choice == 'yes' or choice == 'y':
+       words = ["Lucky af", "Had no luck", "Just feeling lucky", "Unlucky at all"]
+       luck = random.choice(words)
+       print("Today, you may get a title of :", f"{luck}.")
+    elif choice == 'no' or choice == 'n':
+        print("It's a good choice.\nNever do gambling with your luck again!")
+        break
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        continue
