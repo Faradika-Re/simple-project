a35f62f86ef5ff2fce9353ed747f371310fde45c
Fra._da <wemusttellthemthatwecan61@gmail.com>
2025-12-23 23:06:34 +0700

    Replica of Howaboutvisual's Page

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Replica of Howaboutvisual's Page</title>
+    <link rel="shortcut icon" href="source/TUL-Logo_icon.jpg" type="image/x-icon">
+</head>
+<body>
+    
+    <!-- Header Navbar Section -->
+     <header>
+        <ul>
+            <li><a href="">Works</a></li>
+            <li><a href="">Studio</a></li>
+            <li><a href="" id="hav-logo">
+                <b>HAV</b>
+            </a></li>
+            <li><a href="">About</a></li>
+            <li><a href="https://youtube.com/shorts/LctAY9vSCTE?si=36KD4yEmmRBfpa5_" target="_blank">Shop</a></li>
+        </ul>
+        <p class="main-text">
+            CONTRIBUTING TO A <b>BETTER BUSINESS, CULTURE, AND COMMUNITY</b>
+        </p>
+     </header>
+
+     <!-- Main -->
+    <div class="main">
+        <img src="source/HAV_PH_LP0.jpg" alt="Beautiful Beach View">
+        <img src="source/Kura-Kura-Web-2.jpg" alt="Girl and Soda Can">
+        <img src="source/RCL-BANNER-REV.jpg" alt="Some Cool Products">
+
+        <div class="second-text">
+            <p>
+            COLLECTIVE STUDIO WITH GENUINE STRATEGY ESCORTED BY
+        </p>
+        <p>
+            <b>
+                MEANINGFUL CREATIVITY.
+            </b>
+        </p>
+        </div>
+    </div>
+
+    <!-- Footer -->
+     <div class="footer-navbar">
+        <ul>
+            <li>
+                <a href="">Instagram</a>
+            </li>
+            <li>
+                <a href="" class="email">info@testurluck.com</a>
+            </li>
+            <li>
+                <a href="" id="hav-logo">
+                    <b>
+                        HAV
+                    </b>
+                </a>
+            </li>
+            <li>
+                <a href="">+62 821-2194-7003</a>
+            </li>
+            <li>
+                <a href="">Test your luck!</a>
+            </li>
+        </ul>
+     </div>
+</body>
+</html>
